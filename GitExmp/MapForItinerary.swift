082af3dbb84cdb41b149af616fde81f78f9211e3
Sabigya Adhikari <sabigyaadhikari@gmail.com>
2019-09-12 13:20:45 -0400

    Adding map

---
+++ GitExmp/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitExmp
+//
+//  Created by  on 9/12/19.
+//  Copyright © 2019 SabigyaAdhikari. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
